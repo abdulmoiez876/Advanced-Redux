fe77ebca8f67f6a5d308e04b17ca2b0553169e7a
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-02-02 15:46:35 +0500

    remove from cart redux done and cart badge being updated

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,34 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const dummyProducts = [
+    {
+      id: 1,
+      title: 'Test',
+      price: 5,
+      description: 'This is a first product - amazing!'
+    },
+    {
+      id: 2,
+      title: 'Test 2',
+      price: 10,
+      description: 'This is a second product - awesome!'
+    }
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {
+          dummyProducts.map(product => 
+            <ProductItem
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          )
+        }
       </ul>
     </section>
   );
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -10,6 +10,8 @@ const cartSlice = createSlice({
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.cartItems.find(id => id === newItem.id);
+            state.cartTotal ++;
+
             if(!existingItem) {
                 state.cartItems.push({
                     id: newItem.id,
@@ -24,8 +26,22 @@ const cartSlice = createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
             }
         },
-        removeItemFromCart() {
+        removeItemFromCart(state, action) {
+            const idToRemove = action.payload;
 
+            const itemToRemove = state.cartItems.find(id => id === idToRemove);
+            state.cartTotal --;
+
+            if(itemToRemove.quantity === 1) {
+                state.cartItems = state.cartItems.filter(id => id !== idToRemove)
+            }
+            else {
+                itemToRemove.quantity --;
+                itemToRemove.totalPrice = itemToRemove.totalPrice - itemToRemove.price;
+            }
         },
     }
 })
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
